ddae84bccddcc24265e15815722db7f9e6bbfb96
Oliver Butler <olly@protoncloud.co.uk>
2019-05-08 23:33:47 +0100

    All content but JS complete

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 window.onscroll = function () {
-    scrollFunction()
+    scrollFunction();
 };
+
+window.addEventListener("load", highlightSyntax);
 
 function scrollFunction() {
     if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         document.getElementById("floatingButton").style.display = "block";
     }  else {
-        document.getElementById("floatingButton").style.display = "none"
+        document.getElementById("floatingButton").style.display = "none";
     }
 }
 
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+}
+
+function openNav() {
+    document.getElementById("sideNav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+}
+
+function highlightSyntax() {
+    var ca = document.getElementsByClassName("code-html");
+    for(var i = 0; i < ca.length; i++) {
+        var data = ca[i].innerHTML;
+        data = data.replace(/"(.*?)"/g, '<span class="code-str">&quot;$1&quot;</span>');
+        data = data.replace(/&lt;(.*?)&gt;/g, '<span class="code-elem">&lt;$1&gt;</span>');
+        ca[i].innerHTML = data;
+    }
 }
